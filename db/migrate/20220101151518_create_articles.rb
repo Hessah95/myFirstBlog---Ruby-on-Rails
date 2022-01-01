40ccfbe4d39854379a8599b288233ac07a10693e
Hessah95 <hessah.alhamadi.95@gmail.com>
2022-01-01 18:47:56 +0300

    connected all MVC together

---
+++ db/migrate/20220101151518_create_articles.rb
@@ -0,0 +1,10 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps #This method defines two additional columns named created_at and updated_at
+    end
+  end
+end
