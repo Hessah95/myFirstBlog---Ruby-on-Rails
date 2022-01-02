901845b54efec50c78d465d15eebdf847996a841
Hessah95 <hessah.alhamadi.95@gmail.com>
2022-01-02 17:32:21 +0300

    create a new article - DONE

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,26 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # action to get a form for new article
+  def new
+    @article = Article.new
+  end
+
+  # action to create a new article
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unporocessable_entity
+    end
+  end
+
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+
+    # Validations:
+    validates :title, presence: true
+    validates :body, presence: true, length: {minimum: 10}
+    
 end
