6668217d3776ddd28431f44abcb5efb8cfaa2f64
Hessah95 <hessah.alhamadi.95@gmail.com>
2022-01-02 16:38:04 +0300

    show article details - DONE

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,11 @@
 class ArticlesController < ApplicationController
+  # action for the list page
   def index
     @articles = Article.all
+  end
+
+  # action for the details page
+  def show
+    @article = Article.find(params[:id])
   end
 end
